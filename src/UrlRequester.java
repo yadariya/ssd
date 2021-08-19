c3c100b1850788b989c9e4865197313bdb343b44
yadariya <66779337+yadariya@users.noreply.github.com>
2021-08-19 12:19:30 +0300

    exceptions were added

--- src/UrlRequester.java
+++ src/UrlRequester.java
@@ -2,22 +2,24 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class URLrequester {
+public class UrlRequester {
     public static void main(String[] args) {
         try {
             String fileName = args[0];
             String line;
-            BufferedReader bufferreader = new BufferedReader(new FileReader(fileName));
-            while ((line = bufferreader.readLine()) != null) {
+            BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
+            FileWriter file = new FileWriter("output.txt");
+            while ((line = bufferReader.readLine()) != null) {
                 try {
                     URL url = new URL(line);
-                    FileWriter file = new FileWriter("output.txt");
                     BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
                     String lineRead;
                     while ((lineRead = in.readLine()) != null) {
                         file.append(lineRead);
                     }
-                    file.append("---------------------------");
+                    file.append("\n");
+                    file.append("------");
+                    file.append("\n");
                 } catch (MalformedURLException e) {
                     System.out.println("Malformed URL: " + e.getMessage());
 
